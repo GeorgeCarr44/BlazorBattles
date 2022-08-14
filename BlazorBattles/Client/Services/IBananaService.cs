66a874077c03fcddef53d41fe7e88dcad6e63a7e
George Carr <gwcgreen3@gmail.com>
2022-08-14 20:59:18 +0100

    Added Banana Service

---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBattles.Client.Services
+{
+    public interface IBananaService
+    {
+        event Action OnChange;
+
+        int Bananas { get; set; }
+        void EatBananas(int amount);
+        void AddBananas(int amount);
+
+
+    }
+}
